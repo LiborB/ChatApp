e465862b703e862afedda37fc996fea135357060
lbicanovsky <lbicanovsky@logtagrecorders.com>
2020-07-07 14:38:46 +1200

    Changed port to 80 for default

--- client/src/shared/statics.ts
+++ client/src/shared/statics.ts
@@ -1,5 +1,5 @@
 export default class Statics {
     public static Username = "";
     public static CurrentCategory = "";
-    public static baseApiUrl = "http://localhost:3000/";
+    public static baseApiUrl = "/";
 }
